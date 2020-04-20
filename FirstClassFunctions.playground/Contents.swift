09596883e2553f60a8ffe5eaa75e873d1ea89c0a
João Campos <jonybfc@gmail.com>
2020-04-20 15:37:21 +0100

    updating new lines

--- FirstClassFunctions.playground/Contents.swift
+++ FirstClassFunctions.playground/Contents.swift
@@ -4,38 +4,49 @@ import UIKit
 class HeaderView: UIView {
 
     required init?(coder aDecoder: NSCoder) {
+        
         super.init(coder: aDecoder)
     }
 
     init(font: UIFont, textColor: UIColor) {
+        
         super.init(frame: CGRect.zero)
+        
         print("HeaderView with font and color")
     }
 }
 
 class PromotionView: UIView {
+    
     required init?(coder aDecoder: NSCoder) {
+        
         super.init(coder: aDecoder)
     }
 
     init(font: UIFont, textColor: UIColor) {
+        
         super.init(frame: CGRect.zero)
+        
         print("PromotionView with font and color")
     }
 }
 
 class ProfileView: UIView {
+    
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
 
     init(font: UIFont, textColor: UIColor) {
+        
         super.init(frame: CGRect.zero)
+        
         print("ProfileView with font and color")
     }
 }
 
 func call<Input, Output>(_ function: (Input) -> Output, with input: Input) -> Output {
+    
     return function(input)
 }
 
