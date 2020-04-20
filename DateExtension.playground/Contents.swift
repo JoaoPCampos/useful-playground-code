2f88f5b046685d04efb0d758478a1757d19540b6
João Campos <jonybfc@gmail.com>
2020-04-20 15:33:38 +0100

    New lines change

--- DateExtension.playground/Contents.swift
+++ DateExtension.playground/Contents.swift
@@ -2,14 +2,19 @@
 import Foundation
 
 enum DateFormatType: String {
+    
     case ddMMyyy = "dd/MM/yyyy"
 }
 
 extension Date {
+    
     func toString(withFormat format: DateFormatType) -> String {
+        
         let dateFormatter = DateFormatter()
+        
         dateFormatter.locale = Locale.current
         dateFormatter.dateFormat = format.rawValue
+        
         return dateFormatter.string(from: self)
     }
 }
