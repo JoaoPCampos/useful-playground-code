3f81f11118da2395365d4e27b141e1c578fd63e3
João Campos <jonybfc@gmail.com>
2020-04-20 15:56:31 +0100

    Date update new lines

--- StringExtension.playground/Contents.swift
+++ StringExtension.playground/Contents.swift
@@ -2,19 +2,25 @@
 import Foundation
 
 enum DateFormatType: String {
+    
     case ddMMyyy = "dd/MM/yyyy"
 }
 
 extension String {
+    
     func toDate(withFormat format: DateFormatType) -> Date? {
+        
         let dateFormatter = DateFormatter()
+        
         dateFormatter.dateFormat = format.rawValue
         dateFormatter.locale = Locale.current
+        
         return dateFormatter.date(from:self)
     }
 }
 
 //testing
 if let date = "01/01/2000".toDate(withFormat: .ddMMyyy) {
+    
     print(date)
 }
